f6dd249f40523b69f842eb96bc3815c79229516b
Riki Plaza <rikiplaza1@gmail.com>
2022-06-14 02:58:36 -0400

    still needs work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 class App extends React.Component {
-
   render() {
-    return <h1>Gallery of horns</h1>
+    return ( 
+      <header>
+    <h1>Gallery of horns</h1>
+    </header>
+  );
   }
 }
 
