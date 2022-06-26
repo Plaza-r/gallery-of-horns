ed7173a86d2b07aa472bebfa5282156466c9b444
RIKI PLAZA <rikiplaza1@gmail.com>
2022-06-26 04:28:07 -0400

    filter added

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
-
+import './App.css';
 
 class Header extends React.Component {
     render() {
         return (
+            <>
         <header>
             <h1>Gallery of Horns</h1>
         </header>
+        </>
         );  
     }
 }
